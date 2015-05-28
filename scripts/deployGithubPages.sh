c05d9a5f67f2cce703b2cff6af6171577a846296
Christian Kohler <kohler.christian@gmail.com>
2015-05-28 21:45:04 +0200

    Added travis config to deploy github pages

---
+++ scripts/deployGithubPages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rm -rf out || exit 0;
+mkdir out;
+node build.js
+( cd out
+ git init
+ git config user.name "Travis-CI"
+ git config user.email "travis@christiankohler.net"
+ cp ../demo.html ./index.html
+ cp -r ../assets ./
+ cp -r ../dist ./
+ git add .
+ git commit -m "Deployed to Github Pages"
+ git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
